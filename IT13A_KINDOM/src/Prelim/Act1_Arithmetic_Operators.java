3a5bd3fcc21dc7fae98c3128f7f01222d378fed2
misspatriana <kindomkhyleahron@gmail.com>
2025-02-14 10:53:41 +0800

    Java File
    Added HelloWorld class

--- IT13A_KINDOM/src/Prelim/Act1_Arithmetic_Operators.java
+++ IT13A_KINDOM/src/Prelim/Act1_Arithmetic_Operators.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Prelim;
 
-/**
- *
- * @author Administrator
- */
+
 public class Act1_Arithmetic_Operators {
-    
+     public static void main(String[] org){
+             
+        int numb1 = 10;
+        int numb2 = 6;
+        int numb3 = 4;
+        int numb4 = 3; 
+        
+        
+        
+        int kindom1 = numb1 * numb3 + numb2;
+        int backup1 = numb1 - numb3;
+        int kindom2 = backup1 % numb2;
+        int backup2 = numb1 + numb3 + numb2;
+        int kindom3 = backup2 / numb4;
+        int backup3 = numb3 * numb3;
+        int kindom4 = numb1 * numb2 - backup3;
+        
+          System.out.println("10 * 4 + 6 ="+ kindom1);
+         System.out.println("(10-4) % 6 = "+ kindom2);
+            System.out.println("(10 + 4 + 6)/3 = "+ kindom3);
+               System.out.println("10 * 6 -(4 * 4 ) = "+ kindom4);
+               
+     }
 }
+
