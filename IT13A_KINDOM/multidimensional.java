d2cecacfc8ed9682f0e744164fdcb5a71e626c67
KhyleAhronn77 <kindomkhyleahron@gmail.com>
2025-03-25 14:59:47 +0800

    Add files via upload

---
+++ IT13A_KINDOM/multidimensional.java
@@ -0,0 +1,38 @@
+
+package Midterm;
+
+ 
+ 
+//public class multidimensional {
+//     public static void main(String[] args) {
+//       int[][] array = {
+//           {1, 2, 3, 4},
+//           {5, 6, 7, 8},
+//           {9, 10, 11, 12}
+//           
+//       };
+//      
+//    
+//           for (int i = 0; i < array.length; i++){
+//               
+//    System.out.print("The elements in row" + (i + 1) + ":");
+//    for(int j = 0; j < array[i].length; j++)
+//    {
+//        System.out.print (array[i][j] + " ");
+//    }
+//        
+//     System.out.println();
+//    }
+//}
+//               
+//    }         
+
+
+class string {
+ public static void main(String[] args){
+        char[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        for (char i= 0; i< letters.length; i++){
+            
+            System.out.print(letters [i] + " , ");
+            
+            
